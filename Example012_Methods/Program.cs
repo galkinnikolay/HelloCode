69b40f5da7118f16c1e65219e038f3f5c21c708c
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-16 16:39:01 +0300

    Example012_Methods.1

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -60,18 +60,119 @@
 
 
 //Вид 4
-string Method4(int count, string text)
-{
-    int i = 0;
-    string result = String.Empty;
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
 
-    while (i < count)
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// Вид for
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+//====== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленские буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//             + "ежели бы вас послали всместо нашего Винценгероде,"
+//             + "вы бы взяли приступом согласие пруссого короля, "
+//             + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// sting s = "qwerty"
+//            012
+// s[3] // r
+
+// string Replace (string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        Console.Write($"{array[i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
 
-string res = Method4(10, "z");
-Console.WriteLine(res);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i]= array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
