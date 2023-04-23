b696794534b55eb6533b03bfc32440cd8815a3f2
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-23 23:01:53 +0300

    .\Example013_RecursionAlgorithm\

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -133,3 +133,24 @@
 // {
 //     Console.WriteLine($"{i}! = {Factorial(i)}");
 // }
+
+
+
+
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
