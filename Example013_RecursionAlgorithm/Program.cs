e942e4640dc1b09126c1a52901dc97d6a5ad5304
galkinnikolay <galkin_nikolay@icloud.com>
2023-04-23 22:36:13 +0300

    .\Example013_RecursionAlgorithm\

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -90,12 +90,26 @@ void PrintImage(int[,] image)
     {
         for (int j = 0; j < image.GetLength(1); j++)
         {
-            if(image[i;j] == 0) Console.Write($" ");
+            if(image[i,j] == 0) Console.Write($" ");
             else Console.Write($"+");
+
         }
         Console.WriteLine();
     }
 }
 
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    }
+}
 
+PrintImage(pic);
+FillImage(13, 13);
 PrintImage(pic);
